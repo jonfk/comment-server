61413b90ab9eb77b59dc1b81fcf00c037ed56163
Jonathan Fok kan <jfokkan@gmail.com>
2016-11-27 00:36:58 -0500

    go: Added schema for DB

---
+++ go/migrations/up.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE IF NOT EXISTS events (
+       eventId UUID PRIMARY KEY,
+       event_type TEXT NOT NULL,
+       timestamp TIMESTAMP WITH TIME ZONE,
+       data JSONB
+);
+
+
+CREATE TABLE IF NOT EXISTS accounts (
+       account_id UUID PRIMARY KEY,
+       username TEXT UNIQUE NOT NULL,
+       email TEXT UNIQUE,
+       hashed_password BYTEA NOT NULL,
+       hash_salt BYTEA NOT NULL,
+       created_on TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE IF NOT EXISTS comment_threads (
+       comment_thread_id UUID PRIMARY KEY,
+       created_on TIMESTAMP WITH TIME ZONE,
+       page_url TEXT NOT NULL,
+       title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+       comment_id UUID PRIMARY KEY,
+       timestamp TIMESTAMP WITH TIME ZONE,
+       data TEXT,
+       parent_id UUID REFERENCES comments(comment_id),
+       comment_thread_id UUID REFERENCES comment_threads(comment_thread_id),
+       account_id UUID REFERENCES accounts(account_id)
+);
